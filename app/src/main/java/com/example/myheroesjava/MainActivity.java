24bbee652b2e921e39e6751455cc350c37fa782b
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2019-08-08 22:03:22 +0700

    add dynamic title bar

--- app/src/main/java/com/example/myheroesjava/MainActivity.java
+++ app/src/main/java/com/example/myheroesjava/MainActivity.java
@@ -18,6 +18,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     private RecyclerView rvHeroes;
     private ArrayList<Hero> list = new ArrayList<>();
+    private String title = "Mode List";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +50,12 @@ public class MainActivity extends AppCompatActivity {
         rvHeroes.setAdapter(cardViewHeroAdapter);
     }
 
+    private void setActionBarTitle(String title) {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(title);
+        }
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -64,14 +71,20 @@ public class MainActivity extends AppCompatActivity {
     public void setMode(int selectedMode) {
         switch (selectedMode) {
             case R.id.action_list:
+                title = "Mode List";
+                setActionBarTitle(title);
                 showRecyclerList();
                 break;
 
             case R.id.action_grid:
+                title = "Mode Grid";
+                setActionBarTitle(title);
                 showRecyclerGrid();
                 break;
 
             case R.id.action_cardview:
+                title = "Mode CardView";
+                setActionBarTitle(title);
                 showRecyclerCardView();
                 break;
         }
